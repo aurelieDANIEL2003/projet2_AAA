58408a680d05e41a1e2ce4dca153c7fe7fe41e20
-anaëlle- <anaelle.pinceloup@gmail.com>
2024-12-12 16:25:15 +0100

    création fichier streamlit

--- Streamlit.py
+++ Streamlit.py
@@ -3,17 +3,6 @@ from streamlit_option_menu import option_menu
 
 st.title('')
 
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgqfQeBM3AKLr8EtO3qqFcFd_RFgqCd0fCdQ&s);
-    }
-   </style>
-    """,
-    unsafe_allow_html=True
-)
-
 with st.sidebar:
      st.write(f"Bienvenue")
      selection = option_menu(
